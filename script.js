c72ae0237038096673eaed075b358bed313cd1e7
subnauticalaser <123633759+subnauticalaser@users.noreply.github.com>
2024-09-16 13:43:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+class CoreServices extends HTMLElement {
+
+}
+
+
+customElements.define('core-services', CoreServices)
+
+
+
+
+function create() {
+    
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+})
